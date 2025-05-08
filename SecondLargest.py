1d6cac7096320dfb43cad673b1e0c38aa1938149
Hariom Gupta <guptaom244@gmail.com>
2025-05-08 10:56:10 +0530

    1

---
+++ SecondLargest.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        if len(arr)<2:
+            return -1;
+            
+        first = second = -1;
+    
+        for num in arr:
+            if num > first:
+                second = first 
+                first = num
+            elif num > second and num < first :
+                second = num 
+         
+        if second == -1:
+            return -1
+        
+        else :
+            return second
+
